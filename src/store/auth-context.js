1b0db05c08e17e7c3c21bfc5118d1e7ba304362c
kianpas <kianpas@gmail.com>
2021-10-12 23:22:22 +0900

    2021.10.12 로그인 컨텍스트 커밋

---
+++ src/store/auth-context.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token; //!! - tf 에 가까운 내용을 boolean형으로 바꿈, 문자열이 비어있지 않을 경우 T
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
